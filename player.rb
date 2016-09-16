15c825d92b566993fe1314c4e2281ca2898fbc03
App Academy Student <student@appacademy.io>
2016-09-15 18:09:27 -0700

    implement Player class

--- player.rb
+++ player.rb
@@ -3,6 +3,12 @@ class Player
   end
 
   def get_position
-    
+    puts "Please enter a position separated by a comma:"
+    gets.chomp.split(",").map(&:to_i)
+  end
+
+  def get_command
+    puts "Reveal or Flag? (R/F)"
+    gets.chomp
   end
 end
